db9e7af59cc144a8ee850b96e1351ea33e440a9b
EricoGR <ericogr@gmail.com>
2020-08-15 23:52:50 -0300

    add watch to monitor namespace events

--- controllers/tools/autoobjectcreation_controller.go
+++ controllers/tools/autoobjectcreation_controller.go
@@ -18,17 +18,22 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	toolsaocv1 "github.com/ericogr/k8s-aoc/apis/tools/v1"
-	processor "github.com/ericogr/k8s-aoc/pkg/processor"
+)
+
+var (
+	aocOnwerKey = ".metadata.controller"
+	aocGV       = toolsaocv1.GroupVersion.String()
 )
 
 // AutoObjectCreationReconciler reconciles a AutoObjectCreation object
@@ -40,8 +45,27 @@ type AutoObjectCreationReconciler struct {
 
 // SetupWithManager setup
 func (r *AutoObjectCreationReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// if err := mgr.GetFieldIndexer().IndexField(&corev1.Namespace{}, aocOnwerKey, func(rawObj runtime.Object) []string {
+	// 	namespace := rawObj.(*corev1.Namespace)
+	// 	owner := metav1.GetControllerOf(namespace)
+
+	// 	if owner == nil {
+	// 		return nil
+	// 	}
+
+	// 	if owner.APIVersion != aocGV || owner.Kind != "AutoObjectCreation" {
+	// 		return nil
+	// 	}
+
+	// 	return []string{owner.Name}
+	// }); err != nil {
+	// 	return err
+	// }
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&toolsaocv1.AutoObjectCreation{}).
+		Watches(&source.Kind{Type: &corev1.Namespace{}}, &handler.EnqueueRequestForObject{}).
+		// Owns(&corev1.Namespace{}).
 		Complete(r)
 }
 
@@ -50,92 +74,96 @@ func (r *AutoObjectCreationReconciler) SetupWithManager(mgr ctrl.Manager) error 
 
 // Reconcile k8s reconcile
 func (r *AutoObjectCreationReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	ctx := context.Background()
-	log := r.Log.WithValues("autoobjectcreation", req.NamespacedName)
-	var aoc toolsaocv1.AutoObjectCreation
-	err := r.Get(ctx, req.NamespacedName, &aoc)
-
-	if err != nil {
-		if errors.IsNotFound(err) {
-			// Object not found, return. Created objects are automatically garbage collected
-			return ctrl.Result{}, nil
-		}
-
-		// Error reading the object - requeue the request.
-		return ctrl.Result{}, err
-	}
-
-	namespaces, err := r.FindNamespacesByAnnotation(aoc.Spec.Trigger.Annotations)
-
-	processor := processor.Processor{Client: r.Client}
-	var listErrors string
-	for _, namespace := range namespaces {
-		reference := "[" + aoc.Spec.Template.Kind + "(" + aoc.Spec.Template.Name + ")] at " + namespace.ObjectMeta.Name + " namespace"
-		log.Info("Ready to process " + reference)
-
-		newObj, gvk, err := processor.ToObject(aoc.Spec.Template, namespace)
-
-		if err != nil {
-			strErr := "Error serializing " + reference
-			listErrors += strErr + "\n"
-			log.Error(err, strErr)
-			break
-		}
-
-		log.Info("Object encoded succefully " + reference)
-
-		findObj, err := processor.GetObject(
-			*gvk,
-			types.NamespacedName{
-				Namespace: namespace.Name,
-				Name:      aoc.Spec.Template.Name,
-			},
-		)
-
-		if err != nil && errors.IsNotFound(err) {
-			log.Info("Creating new object " + reference)
-			err := r.Client.Create(ctx, &newObj)
-
-			if err == nil {
-				log.Info("Create succefully " + reference)
-			} else {
-				strErr := "Error creating object " + reference
-				listErrors += strErr + "\n"
-				log.Error(err, strErr)
-			}
-		} else {
-			if err == nil {
-				findObj.Object["spec"] = newObj.Object["spec"]
-				err := r.Client.Update(ctx, &findObj)
-
-				if err == nil {
-					log.Info("Update succefully " + reference)
-				} else {
-					strErr := "Error updating object " + reference
-					listErrors += strErr + "\n"
-					log.Error(err, strErr)
-				}
-			} else {
-				strErr := "Error getting object " + reference
-				listErrors += strErr + "\n"
-				log.Error(err, strErr)
-			}
-		}
-	}
-
-	// //https://godoc.org/sigs.k8s.io/controller-runtime/pkg/predicate#GenerationChangedPredicate
-	if listErrors != "" {
-		aoc.Status.Status = listErrors
-	} else {
-		aoc.Status.Status = "OK"
-	}
-
-	if err := r.Status().Update(ctx, &aoc); err != nil {
-		log.Error(err, "Unable to update status")
-		return ctrl.Result{}, err
-	}
-
+	fmt.Println("-----------------------")
+	fmt.Println(req.NamespacedName.Name)
+	fmt.Println("-----------------------")
 	return ctrl.Result{}, nil
+
+	// ctx := context.Background()
+	// log := r.Log.WithValues("autoobjectcreation", req.NamespacedName)
+	// var aoc toolsaocv1.AutoObjectCreation
+	// err := r.Get(ctx, req.NamespacedName, &aoc)
+
+	// if err != nil {
+	// 	if errors.IsNotFound(err) {
+	// 		// Object not found, return. Created objects are automatically garbage collected
+	// 		return ctrl.Result{}, nil
+	// 	}
+
+	// 	// Error reading the object - requeue the request.
+	// 	return ctrl.Result{}, err
+	// }
+
+	// namespaces, err := r.FindNamespacesByAnnotation(aoc.Spec.Trigger.Annotations)
+	// processor := processor.Processor{Client: r.Client}
+	// var listErrors string
+	// for _, namespace := range namespaces {
+	// 	reference := "[" + aoc.Spec.Template.Kind + "(" + aoc.Spec.Template.Name + ")] at " + namespace.ObjectMeta.Name + " namespace"
+	// 	log.Info("Ready to process " + reference)
+
+	// 	newObj, gvk, err := processor.ToObject(aoc.Spec.Template, namespace)
+
+	// 	if err != nil {
+	// 		strErr := "Error serializing " + reference
+	// 		listErrors += strErr + "\n"
+	// 		log.Error(err, strErr)
+	// 		break
+	// 	}
+
+	// 	log.Info("Object encoded succefully " + reference)
+
+	// 	findObj, err := processor.GetObject(
+	// 		*gvk,
+	// 		types.NamespacedName{
+	// 			Namespace: namespace.Name,
+	// 			Name:      aoc.Spec.Template.Name,
+	// 		},
+	// 	)
+
+	// 	if err != nil && errors.IsNotFound(err) {
+	// 		log.Info("Creating new object " + reference)
+	// 		err := r.Client.Create(ctx, &newObj)
+
+	// 		if err == nil {
+	// 			log.Info("Create succefully " + reference)
+	// 		} else {
+	// 			strErr := "Error creating object " + reference
+	// 			listErrors += strErr + "\n"
+	// 			log.Error(err, strErr)
+	// 		}
+	// 	} else {
+	// 		if err == nil {
+	// 			findObj.Object["spec"] = newObj.Object["spec"]
+	// 			err := r.Client.Update(ctx, &findObj)
+
+	// 			if err == nil {
+	// 				log.Info("Update succefully " + reference)
+	// 			} else {
+	// 				strErr := "Error updating object " + reference
+	// 				listErrors += strErr + "\n"
+	// 				log.Error(err, strErr)
+	// 			}
+	// 		} else {
+	// 			strErr := "Error getting object " + reference
+	// 			listErrors += strErr + "\n"
+	// 			log.Error(err, strErr)
+	// 		}
+	// 	}
+	// }
+
+	// // //https://godoc.org/sigs.k8s.io/controller-runtime/pkg/predicate#GenerationChangedPredicate
+	// if listErrors != "" {
+	// 	aoc.Status.Status = listErrors
+	// } else {
+	// 	aoc.Status.Status = "OK"
+	// }
+
+	// if err := r.Status().Update(ctx, &aoc); err != nil {
+	// 	log.Error(err, "Unable to update status")
+	// 	return ctrl.Result{}, err
+	// }
+
+	// return ctrl.Result{}, nil
 }
 
 // FindNamespaces find all namespaces
