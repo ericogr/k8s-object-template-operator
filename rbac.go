df80c6b167b332cca7e278a3aa4e84c02914c42a
EricoGR <ericogr@gmail.com>
2020-08-19 23:45:19 -0300

    create aditional rbac file

---
+++ rbac.go
@@ -0,0 +1,7 @@
+package main
+
+// -----------------------------------
+// put extra rbac configurations here
+// -----------------------------------
+
+// +kubebuilder:rbac:groups=monitoring.coreos.com,resources=prometheusrules,verbs=get;list;create;update;patch
